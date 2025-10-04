b2c99f0ae4a2449c13b37ac53821358f865d5476
StateWatcher <maksim19032007@gmail.com>
2025-10-04 12:31:21 +0300

    feat(binary utils): add module with binary utils

---
+++ src/binary-utils.cpp
@@ -0,0 +1,115 @@
+#include <string>
+
+std::string intToBinaryString(int intNum)
+{
+  std::string binaryString;
+  unsigned int mask = 1u << 31;
+
+  for (int bit = 0; bit < 32; bit++)
+  {
+    binaryString += (intNum & mask ? '1' : '0');
+    mask >>= 1;
+  }
+
+  return binaryString;
+}
+
+
+std::string floatToBinaryString(float floatNum)
+{
+  union
+  {
+    float floatRepresentation;
+    unsigned int intRepresentation;
+  } floatToInt;
+  floatToInt.floatRepresentation = floatNum;
+
+  std::string binaryString;
+  unsigned int mask = 1u << 31;
+
+  for (int bit = 0; bit < 32; bit++)
+  {
+    binaryString += (floatToInt.intRepresentation & mask ? '1' : '0');
+    mask >>= 1;
+  }
+
+  return binaryString;
+}
+
+
+std::string doubleToBinaryString(double doubleNum)
+{
+  union
+  {
+    double doubleRepresentation;
+    unsigned long long intRepresentation;
+  } doubleToInt;
+
+  doubleToInt.doubleRepresentation = doubleNum;
+
+  std::string binaryString;
+  unsigned long long mask = 1ULL << 63;
+
+  for (int bit = 0; bit < 64; bit++)
+  {
+    binaryString += (doubleToInt.intRepresentation & mask ? '1' : '0');
+    mask >>= 1;
+  }
+
+  return binaryString;
+}
+
+
+int changeBitInt(int number, int bitPosition, int newValue) {
+    int realPosition = 31 - bitPosition;
+    unsigned int mask = 1u << realPosition;
+    
+    if (newValue == 1) {
+        number |= mask;
+    } else {
+        number &= ~mask;
+    }
+    
+    return number;
+}
+
+float changeBitFloat(float number, int bitPosition, int newValue) {
+    union {
+        float floatRepresentation;
+        unsigned int intRepresentation;
+    } floatToInt;
+    
+    floatToInt.floatRepresentation = number;
+    
+    int realPosition = 31 - bitPosition;
+    unsigned int mask = 1u << realPosition;
+    
+    if (newValue == 1) {
+        floatToInt.intRepresentation |= mask;
+    } else {
+        floatToInt.intRepresentation &= ~mask;
+    }
+    
+    return floatToInt.floatRepresentation;
+}
+
+// Изменение бита в double числе
+double changeBitDouble(double number, int bitPosition, int newValue) {
+    union {
+        double doubleRepresentation;
+        unsigned long long intRepresentation;
+    } doubleToInt;
+    
+    doubleToInt.doubleRepresentation = number;
+    
+    int realPosition = 63 - bitPosition;
+    unsigned long long mask = 1ULL << realPosition;
+    
+    if (newValue == 1) {
+        doubleToInt.intRepresentation |= mask;
+    } else {
+        doubleToInt.intRepresentation &= ~mask;
+    }
+    
+    return doubleToInt.doubleRepresentation;
+}
