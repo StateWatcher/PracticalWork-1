2c1082674ec64dc51ceb40f13350027455f51348
StateWatcher <maksim19032007@gmail.com>
2025-10-04 12:32:25 +0300

    feat(main): add entry point main file

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "laboratory-tasks.h"
+#include "console-utils.h"
+
+int main()
+{
+  clearConsole();
+  while (true)
+  {
+      int selectedTask;
+      std::cout << "Введите номер задания для выполнения:\n"
+                << "1.Сколько памятии занимают типы данных.\n"
+                << "2.Двоичное представление введённого int\n"
+                << "3.Двоичное представление введённого float\n"
+                << "4.Двоичное представление введённого double\n"
+                << "5.Замена бита в числе\n";
+      std::cin >>
+          selectedTask;
+      if (selectedTask < 1 || selectedTask > 5 || std::cin.fail())
+          break;
+      clearConsole();
+      switch (selectedTask)
+      {
+      case 1:
+        task1();
+        break;
+      case 2:
+        task2();
+        break;
+      case 3:
+        task3();
+        break;
+      case 4:
+        task4();
+        break;
+      case 5:
+        task5();
+        break;
+      default:
+        break;
+      }
+  }
+  return 0;
+}
